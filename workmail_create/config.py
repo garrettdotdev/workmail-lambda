0da0a78186f95054b82b36cb06210530a9b0df9d
Garrett Rohde <garrettrohde86@gmail.com>
2024-12-15 14:40:56 -0500

    Refactor `::get_config()`

--- workmail_create/config.py
+++ workmail_create/config.py
@@ -2,23 +2,24 @@
 import os
 
 
-def get_env_variable(key, default=None, env=os.environ):
-    value = os.getenv(key, default)
-    if value is None:
-        raise ValueError(f"Environment variable {key} is required")
-    return value
-
-
-def get_config(env=os.environ):
-    return {
-        "DB_SECRET_ARN": get_env_variable("DB_SECRET_ARN", env=env),
-        "DB_CLUSTER_ARN": get_env_variable("DB_CLUSTER_ARN", env=env),
-        "DATABASE_NAME": get_env_variable("DATABASE_NAME", env=env),
-        "ORGANIZATION_ID": get_env_variable("ORGANIZATION_ID", env=env),
-        "KEAP_BASE_URL": get_env_variable("KEAP_BASE_URL", env=env),
-        "KEAP_API_KEY": get_env_variable("KEAP_API_KEY", env=env),
-        "KEAP_TAG": get_env_variable("KEAP_TAG", env=env),
-        "SNS_BOUNCE_ARN": get_env_variable("SNS_BOUNCE_ARN", env=env),
-        "SNS_COMPLAINT_ARN": get_env_variable("SNS_COMPLAINT_ARN", env=env),
-        "SNS_DELIVERY_ARN": get_env_variable("SNS_DELIVERY_ARN", env=env),
-    }
+def get_config():
+    required_vars = [
+        "DB_SECRET_ARN",
+        "DB_CLUSTER_ARN",
+        "DATABASE_NAME",
+        "SNS_BOUNCE_ARN",
+        "SNS_COMPLAINT_ARN",
+        "SNS_DELIVERY_ARN",
+        "KEAP_BASE_URL",
+        "KEAP_API_KEY",
+        "KEAP_TAG",
+    ]
+    config = {}
+    for var in required_vars:
+        value = os.environ.get(var)
+        if not value:
+            raise EnvironmentError(
+                f"Environment variable {var} is required but not set."
+            )
+        config[var] = value
+    return config
