5155e89b2829b8e23a4ab7685b56e69895f853f9
Garrett Rohde <garrettrohde86@gmail.com>
2024-12-15 14:42:27 -0500

    Refactor

--- workmail_cancel/app.py
+++ workmail_cancel/app.py
@@ -1,58 +1,46 @@
 import json
 import os
-import jsonschema
-from jsonschema import validate
 import boto3
 import logging
+import uuid
+from botocore.config import Config
 from botocore.exceptions import ClientError, BotoCoreError
 from typing import Dict, Any
 from workmail_cancel.config import get_config
-from workmail_common.utils import load_schema, handle_error
+from workmail_common.utils import (
+    handle_error,
+    process_input,
+    connect_to_rds,
+    get_aws_clients,
+)
 
 # Initialize logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def get_aws_clients(region_name: str) -> Dict[str, Any]:
-    return {
-        "rds_client": boto3.client("rds-data", region_name=region_name),
-        "cloudformation_client": boto3.client(
-            "cloudformation", region_name=region_name
-        ),
-    }
-
-
-def query_workmail_stack(
-    contact_id: int, appname: str, rds_client: Any, config: Dict[str, str]
+def get_workmail_organization_id(
+    contact_id: int, vanity_name: str, connection: Any
 ) -> str:
     try:
-        db_secret_arn = config["DB_SECRET_ARN"]
-        db_cluster_arn = config["DB_CLUSTER_ARN"]
-        database_name = config["DATABASE_NAME"]
+        cursor = connection.cursor()
         sql = """
-        SELECT stackid
-        FROM workmail_stacks
-        WHERE ownerid = :contact_id
+        SELECT organization_id
+        FROM workmail_organizations
+        WHERE ownerid = %s
+        AND vanity_name = %s
         LIMIT 1
         """
-        response = rds_client.execute_statement(
-            secretArn=db_secret_arn,
-            resourceArn=db_cluster_arn,
-            sql=sql,
-            database=database_name,
-            parameters=[
-                {"name": "contact_id", "value": {"longValue": contact_id}},
-                {"name": "appname", "value": {"stringValue": appname}},
-            ],
-        )
-        if not response["records"]:
+        cursor.execute(sql, (contact_id, vanity_name))
+        result = cursor.fetchone()
+
+        if not result:
             raise ValueError(
-                f"No WorkMail stack found with contact_id={contact_id} and appname={appname}"
+                f"No WorkMail organization_id found with contact_id={contact_id} and vanity_name={vanity_name}"
             )
-        stack_id = response["records"][0][0]["stringValue"]
-        logger.info(f"Found WorkMail stack with stack_id={stack_id}")
-        return stack_id
+        organization_id = result
+        logger.info(f"Found WorkMail stack with stack_id={organization_id}")
+        return organization_id
     except (ClientError, BotoCoreError) as e:
         logger.error(f"Failed to query RDS: {e}")
         raise
@@ -61,15 +49,28 @@ def query_workmail_stack(
         raise
 
 
-def delete_workmail_stack(stack_id: str, cloudformation_client: Any) -> None:
+def delete_workmail_organization(
+    organization_id: str, workmail_client: Any
+) -> Dict[str, Any]:
     try:
-        cloudformation_client.delete_stack(StackName=stack_id)
-        logger.info(f"Initiated deletion of WorkMail stack with stack_id={stack_id}")
+        logger.info(
+            f"Deleting WorkMail organization with organization_id={organization_id}"
+        )
+        delete_organization_response = workmail_client.delete_organization(
+            ClientToken=str(uuid.uuid4()),
+            OrganizationId=organization_id,
+            DeleteDirectory=True,
+            ForceDelete=True,
+        )
+        logger.info(
+            f"Delete response: {json.dumps(delete_organization_response, default=str)}"
+        )
+        return delete_organization_response
     except (ClientError, BotoCoreError) as e:
-        logger.error(f"Failed to delete WorkMail stack: {e}")
+        logger.error(f"Failed to delete WorkMail organization: {e}")
         raise
     except Exception as e:
-        logger.error(f"Unexpected error deleting WorkMail stack: {e}")
+        logger.error(f"Unexpected error deleting WorkMail organization: {e}")
         raise
 
 
@@ -78,26 +79,32 @@ def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
         config = get_config()
         aws_clients = get_aws_clients(region_name="us-east-1")
 
-        pwd = os.path.dirname(os.path.abspath(__file__))
-        schema_path = f"{pwd}/schemas/input_schema.json"
-        input_schema = load_schema(schema_path)
-
         body = json.loads(event["body"])
-        validate(instance=body, schema=input_schema)
 
-        contact_id = body["contact_id"]
-        appname = body["appname"]
-        vanity_name = body["vanity_name"]
+        pwd = os.path.dirname(os.path.abspath(__file__))
+        schema_path = os.path.join(pwd, "/schemas/input_schema.json")
+        clean_input = process_input(body, schema_path)
 
-        stack_id = query_workmail_stack(
-            contact_id, appname, aws_clients["rds_client"], config=config
+        contact_id = clean_input["contact_id"]
+        vanity_name = clean_input["vanity_name"]
+
+        organization_id = get_workmail_organization_id(
+            contact_id, vanity_name, aws_clients["rds_client"], config=config
         )
-        delete_workmail_stack(stack_id, aws_clients["cloudformation_client"])
+        delete_workmail_organization_response = delete_workmail_organization(
+            organization_id, aws_clients["cloudformation_client"]
+        )
 
         return {
             "statusCode": 200,
             "body": json.dumps(
-                {"message": "WorkMail stack deletion initiated.", "stackId": stack_id}
+                {
+                    "message": "Deleted workmail organization.",
+                    "organization_id": delete_workmail_organization_response[
+                        "OrganizationId"
+                    ],
+                    "state": delete_workmail_organization_response["State"],
+                }
             ),
         }
     except Exception as e:
