d1eb7af6a01b5fbb2a64cb9632d516801d529a1e
Garrett Rohde <garrettrohde86@gmail.com>
2024-12-17 21:29:45 -0500

    Add workmail_authorizer

---
+++ authorizer_function/app.py
@@ -0,0 +1,45 @@
+# authorizer_function/app.py
+import json
+import logging
+import os
+from workmail_common.utils import (
+    get_aws_clients,
+    handle_error,
+    get_secret,
+)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    """Main handler for Api Gateway authorizer"""
+    logger.info(f"Received request: " + json.dumps(event))
+
+    token = event["headers"].get("Authorization")
+    if not token:
+        logger.warning("No token provided")
+        raise Exception("Unauthorized")
+
+    # Remove 'Bearer ' from token if present
+    if token.lower.startswith("bearer "):
+        token = token[7:]
+
+    try:
+        secret_name = os.getenv("TOKEN_SECRET_NAME")
+        if not secret_name:
+            logger.error("TOKEN_SECRET_NAME environment variable not set")
+            raise Exception("Internal Server Error")
+
+        secret = get_secret(secret_name)
+
+        if token != secret:
+            logger.warning("Invalid token")
+            raise Exception("Unauthorized")
+
+        return {
+            "isAuthorized": True,
+        }
+
+    except Exception as e:
+        handle_error(e)
