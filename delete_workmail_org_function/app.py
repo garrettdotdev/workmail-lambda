7ef57502ef1d0689f526ae936da5318d38d9bdaf
Garrett Rohde <garrettrohde86@gmail.com>
2024-12-26 19:17:56 -0500

    Merge config into app

--- delete_workmail_org_function/app.py
+++ delete_workmail_org_function/app.py
@@ -6,7 +6,6 @@ import uuid
 from botocore.config import Config
 from botocore.exceptions import ClientError, BotoCoreError
 from typing import Dict, Any
-from delete_workmail_org_function.config import get_config
 from workmail_common.utils import (
     handle_error,
     process_input,
@@ -17,6 +16,26 @@ from workmail_common.utils import (
 # Initialize logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+
+def get_config():
+    required_vars = [
+        "DB_SECRET_ARN",
+        "DB_CLUSTER_ARN",
+        "DATABASE_NAME",
+        "SNS_BOUNCE_ARN",
+        "SNS_COMPLAINT_ARN",
+        "SNS_DELIVERY_ARN",
+    ]
+    config = {}
+    for var in required_vars:
+        value = os.environ.get(var)
+        if not value:
+            raise EnvironmentError(
+                f"Environment variable {var} is required but not set."
+            )
+        config[var] = value
+    return config
 
 
 def get_workmail_organization_id(
